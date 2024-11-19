9e6e0bdc2b9571b019d80c915d9c25045e17a909
Mase S <masesafi@gmail.com>
2024-11-19 02:10:17 +0100

    Har skapat en todo lista och skrivit vilka klasser och funktioner jag ska ha

---
+++ todo.cs
@@ -0,0 +1,17 @@
+//Klasser:Kort,Atm,konto,transaktion
+
+//Kort klassen ska innehålla pin-kod,kortdetaljer(nummer),spärrstatus
+
+//Transaktions klassen innehåller all data om vad som sker vid en transaktion alltså datum,belopp,Uttag/intag
+
+//Konto klassen ska hantera saldot och transaktionerna som har koppling till ett kort
+
+//Atm klassen ska ha logiken för bankomaten,hantera kort samt kontouppgifter
+
+// Funktioner jag vill ha med: 
+//Låsa kontot om man anger fel 3 gånger
+//lösenordet ska vara skyddad
+//spara transaktionshistorik
+//ta ut pengar
+//funktion så att man kan slå in pinkod 
+//sätta in pengar
