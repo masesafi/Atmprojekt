7e1027f6bd0dc86bc667ba8ed061555216a140eb
Mase S <masesafi@gmail.com>
2024-11-21 20:28:43 +0100

    La till while loopar och funktioner som hjälper en att logga in eller skapa konto

--- Program.cs
+++ Program.cs
@@ -1,47 +1,85 @@
 using system;
 public class Program
 {
+
     public static void Main (string[] args)
     {
+        while(true)
+        {
         Console.WriteLine("Välkommen till modern Atm");
         Console.WriteLine("1. Logga in. ");
         Console.WriteLine("2. Skapa konto.");
         Console.WriteLine("3. Avsluta");
         Console.WriteLine("4. Välj ett av alternativen ovan:");
+
+        try
         {
-            int choice = int.parse(Console.ReadLine());
+
+            int choice = int.Parse(Console.ReadLine());
 
             switch (choice)
             {
                 case 1:
-               Console.WriteLine("Du är nu inloggad"); 
+               HandleLogin();
                break;
                case 2:
-               Console.WriteLine("Du valde att skapa ett nytt konto");
+               HandleCreateAccount();
                break;
                case 3:
                Console.WriteLine("Du valde att avsluta, på återseende!");
-               environment.Exit(0);
+               Environment.Exit(0);
                break;
                default;
                Console.WriteLine("Ogiltigt val, vänligen försök igen");
                break;
-
-                
-
             }
-
-            
-
-
         }
-
-
-
-
+        catch(FormatException)
+        {
+            Console.WriteLine("Vänligen ange ett nummer (1-3)");
+         }
+        }
     }
 
-}
+            static void HandleLogin()
+            {
+            Console.WriteLine("Ange ditt kortnummer: ");
+            string cardNumber = Console.ReadLine();
+            
+            Console.WriteLine("Ange din PIN-kod: ");
+            string Pin = Console.ReadLine();
+            }
+        }
+
+        static void HandleCreateAccount()
+        {
+            Console.WriteLine("Ange ditt förnamn");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("Ange ditt efternamn");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine("Skapa din pin kod (4 siffror)");
+            string Pin = Console.ReadLine();
+            
+            while (Pin.Length != 4 || pin.Contains(" ") || !Pin.All(char.IsDigit))
+            {
+                Console.WriteLine("Ogiltig kod, din pin kod måste vara exakt 4 siffror. Försök igen.");
+                pin = Console.ReadLine();
+                
+            }
+
+
+
+            
+            
+        }
+
+        
+        
+        
+            
+        
 
 
 
@@ -77,5 +115,7 @@ public class Program
 
 
 
-}
+
+
+
     
