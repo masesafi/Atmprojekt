bec50d059fa5b98509a834800d6a466f746af223
Mase S <masesafi@gmail.com>
2024-11-27 04:33:23 +0100

    Skapade egenskaper för kontoinnehave samt lagt till konstruktor även listaför alla kontoinnehavare

--- account.cs
+++ account.cs
@@ -6,3 +6,25 @@ public static string GenerateCardNumber()
     return ($"{random.next(1000, 9999)}-{random.Next(1000, 9999)}-{random.Next(1000, 9999)}-{random.Next(1000, 9999)}")
 }
 
+public string FirstName = {get; set;}
+public string LastName = {get; set;}
+public int CardNumber = {get; set;}
+public string Pin = {get; set;}
+public decimal Balance = { get; set;}
+
+
+
+    public static List<Account> Accounts = new List<Account>();
+{
+
+    public Account(string FirstName, string LastName, int CardNumber, string Pin)
+
+    FirstName = firstName
+    LastName = lastName
+    Pin = pin;
+    Balance = 0; //Detta så att alla konton börjar på 0 kronor
+
+    Account.Add(this);
+
+}
+
