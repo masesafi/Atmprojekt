f166a6ba563c4e3bb96bfaa92c6d39952d0525fe
Mase S <masesafi@gmail.com>
2024-12-08 17:45:19 +0100

    lade till lista i account klassen som ska lagra alla transactioner som sker

--- account.cs
+++ account.cs
@@ -1,32 +1,35 @@
 using System;
 using System.Collections.Generic;
+using System.Transactions;
 
 namespace atmprojekt
 {
-public class Account
-{
-public string FirstName {get; set;}
-public string LastName {get; set;}
-public string CardNumber  {get; set;}
-public string Pin {get; set;}
-public decimal Balance { get; set;}
-    public static List<Account> Accounts = new List<Account>();
-
-    public static string GenerateCardNumber()
-{
-    Random random = new Random();
-    return $"{random.Next(1000, 9999)}-{random.Next(1000, 9999)}-{random.Next(1000, 9999)}-{random.Next(1000, 9999)}";
-}
-
-    public Account(string firstName, string lastName, string pin)
+    public class Account
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CardNumber { get; set; }
+        public string Pin { get; set; }
+        public decimal Balance { get; set; }
+        public static List<Account> Accounts = new List<Account>();
 
-    FirstName = firstName;
-    LastName = lastName;
-    CardNumber = GenerateCardNumber();
-    Pin = pin;
-    Balance =0; //Detta så att alla konton börjar på 0 kronor
-     Accounts.Add(this);
+        public static string GenerateCardNumber()
+        {
+            Random random = new Random();
+            return $"{random.Next(1000, 9999)}-{random.Next(1000, 9999)}-{random.Next(1000, 9999)}-{random.Next(1000, 9999)}";
+        }
+
+        public Account(string firstName, string lastName, string pin)
+        {
+
+            FirstName = firstName;
+            LastName = lastName;
+            CardNumber = GenerateCardNumber();
+            Pin = pin;
+            Balance = 0; //Detta så att alla konton börjar på 0 kronor
+            Accounts.Add(this);
+        }
+
+        public List<Transaction> Transactions { get; set; } = new List<Transaction>();
     }
-}
 }
