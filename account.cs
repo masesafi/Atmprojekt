413b9782b0d0de4735903ebaf42dfcfaef66fb6d
Mase S <masesafi@gmail.com>
2024-12-09 11:32:41 +0100

    Tog bort generate card från konstruktorn då den gjorde att det blev 2 kontonummer skapade

--- account.cs
+++ account.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Transactions;
 
 namespace atmprojekt
@@ -13,21 +14,35 @@ namespace atmprojekt
         public decimal Balance { get; set; }
         public static List<Account> Accounts = new List<Account>();
 
+
+    
+       private static Random random = new Random();
         public static string GenerateCardNumber()
         {
-            Random random = new Random();
+            
             return $"{random.Next(1000, 9999)}-{random.Next(1000, 9999)}-{random.Next(1000, 9999)}-{random.Next(1000, 9999)}";
         }
 
-        public Account(string firstName, string lastName, string CardNumber, string pin)
+        public Account(string firstName, string lastName, string cardNumber, string pin)
         {
 
             FirstName = firstName;
             LastName = lastName;
-            CardNumber = GenerateCardNumber();
+            CardNumber = cardNumber;
             Pin = pin;
             Balance = 0; //Detta så att alla konton börjar på 0 kronor
             Accounts.Add(this);
+            Console.WriteLine($"Konto har tillagts i listan: {FirstName} {LastName}, Kortnummer: {CardNumber}");
+
+        }
+
+        public static void ListAllAccounts()
+        {
+            Console.WriteLine("Konton i systemet:");
+            foreach (var account in Accounts)
+            {
+                Console.WriteLine($"Namn: {account.FirstName} {account.LastName}, Kortnummmer: {account.CardNumber}, Pin; {account.Pin}");
+            }
         }
 
         public List<Transaction> Transactions { get; set; } = new List<Transaction>();
